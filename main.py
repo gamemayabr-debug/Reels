5d89ab19dd14dabbdc523edbef1a0c2ee041d599
gamemayabr-debug <gamemaya.br@gmail.com>
2026-03-18 14:50:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import requests
 import os
-TOKEN = os.getenv("8704824778:AAF1ISSK3UAYdvuHki6IriA-LDeVMlrnA6c")
-CHAT_ID = os.getenv("447181422")
+
+TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 message = "Тест: всё работает"
 
 url = f"https://api.telegram.org/bot{8704824778:AAF1ISSK3UAYdvuHki6IriA-LDeVMlrnA6c}/sendMessage"
 
 requests.post(url, data={
-    "chat_id": CHAT_ID,
-    "text": message
+    "chat_id": 447181422,
+    "text": все работает
 })
